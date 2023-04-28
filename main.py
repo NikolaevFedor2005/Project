f4bbcb2c9371f24ed50f179596c67d47c73b1029
NikolaevFedor2005 <118173420+NikolaevFedor2005@users.noreply.github.com>
2023-04-28 09:38:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,119 @@
-import sys
+from flask import Flask, request, jsonify
+import logging
+import json
+import random
 
+app = Flask(__name__)
+
+logging.basicConfig(level=logging.INFO)
+
+# создаем словарь, в котором ключ — название города,
+# а значение — массив, где перечислены id картинок,
+# которые мы записали в прошлом пункте.
+
+cities = {
+    'Past lives, BRNS': ['<speaker audio="dialogs-upload/39ac0c0b-3802-4877-8e5e-0cd8c0fa7b9d/'
+                         'f7732806-d8a8-4039-89cd-9489b809803f.opus">'],
+    'Empty dreams, Cypariss': ['<speaker audio="dialogs-upload/39ac0c0b-3802'
+                               '-4877-8e5e-0cd8c0fa7b9d/652a93b0-42aa-4d7f-9ce2-78ace96aed20.opus">']
+}
+
+# создаем словарь, где для каждого пользователя
+# мы будем хранить его имя
+sessionStorage = {}
+
+
+@app.route('/post', methods=['POST'])
+def main():
+    logging.info(f'Request: {request.json!r}')
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+    handle_dialog(response, request.json)
+    logging.info(f'Response: {response!r}')
+    return jsonify(response)
+
+
+def handle_dialog(res, req):
+    user_id = req['session']['user_id']
+
+    # если пользователь новый, то просим его представиться.
+    if req['session']['new']:
+        res['response']['text'] = 'Привет! Назови свое имя!'
+        # создаем словарь в который в будущем положим имя пользователя
+        sessionStorage[user_id] = {
+            'first_name': None
+        }
+        return
+
+    # если пользователь не новый, то попадаем сюда.
+    # если поле имени пустое, то это говорит о том,
+    # что пользователь еще не представился.
+    if sessionStorage[user_id]['first_name'] is None:
+        # в последнем его сообщение ищем имя.
+        first_name = get_first_name(req)
+        # если не нашли, то сообщаем пользователю что не расслышали.
+        if first_name is None:
+            res['response']['text'] = \
+                'Не расслышала имя. Повтори, пожалуйста!'
+        # если нашли, то приветствуем пользователя.
+        # И спрашиваем какой город он хочет увидеть.
+        else:
+            sessionStorage[user_id]['first_name'] = first_name
+            res['response'][
+                'text'] = 'Приятно познакомиться, ' \
+                          + first_name.title() \
+                          + '. Я - Алиса. Какю песню хочешь послушать?'
+            # получаем варианты buttons из ключей нашего словаря cities
+            res['response']['buttons'] = [
+                {
+                    'title': city.title(),
+                    'hide': True
+                } for city in cities
+            ]
+    # если мы знакомы с пользователем и он нам что-то написал,
+    # то это говорит о том, что он уже говорит о треке,
+    # что хочет увидеть.
+    else:
+        # ищем трек в сообщение от пользователя
+        city = get_city(req)
+        # если этот трек среди известных нам,
+        # то воспроизводим его
+        if city in cities:
+            res['response']['tts'] = {}
+            res['response']['tts']['title'] = 'Этот трек я знаю.'
+            res['response']['tts']['image_id'] = cities[city]
+        # если не нашел, то отвечает пользователю
+        # 'Первый раз слышу об этом треке.'
+        else:
+            res['response']['text'] = \
+                'Первый раз слышу об этом треке. Попробуй еще разок!'
+
+
+def get_city(req):
+    # перебираем именованные сущности
+    for entity in req['request']['nlu']['entities']:
+        # если тип YANDEX.GEO то пытаемся получить город(city),
+        # если нет, то возвращаем None
+        if entity['type'] == 'YANDEX.GEO':
+            # возвращаем None, если не нашли сущности с типом YANDEX.GEO
+            return entity['value'].get('city', None)
+
+
+def get_first_name(req):
+    # перебираем сущности
+    for entity in req['request']['nlu']['entities']:
+        # находим сущность с типом 'YANDEX.FIO'
+        if entity['type'] == 'YANDEX.FIO':
+            # Если есть сущность с ключом 'first_name',
+            # то возвращаем ее значение.
+            # Во всех остальных случаях возвращаем None.
+            return entity['value'].get('first_name', None)
+
+
+if __name__ == '__main__':
+    app.run()
